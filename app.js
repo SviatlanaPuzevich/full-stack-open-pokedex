9addb7484fb630a6c716bf2e53e9d4cb75ba1ff7
puzevich <s.rudaya@sam-solutions.com>
2025-08-01 23:57:50 +0300

    exercise 11.19

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/dead', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)// eslint-disable-line
 })
